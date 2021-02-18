84ff903ecdf7932e775d39f6b711f135a51444ec
Marija Rumenovska <marija.rumenovska@hotmail.com>
2021-02-18 10:43:27 +0100

    cSharp Ass

---
+++ AssCSharp/ClassLibrary/EventHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class HttpHandller
+    {
+        public event EventHandler WebContentReturned;
+        protected virtual void onWebContentReturned(HttpResponseArgs e) {
+            EventHandler handler = WebContentReturned;
+            handler?.Invoke(this, e);
+
+        
+        }
+        public delegate void WebContentReturnedEventHandler(object sender, HttpResponseArgs e);
+
+    }
+}
---
+++ AssCSharp/ClassLibrary/HttpResponseArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary
+{
+    public class HttpResponseArgs : EventArgs
+    {
+        public string HttpBody { get; set; }
+        public int ResponseCode { get; set; }
+
+    }
+}
